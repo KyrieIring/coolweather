fc74c48243b59d0cd7ae0fdfe96069d360bd97a9
kyrie <501006627@qq.com>
2018-06-11 20:20:59 +0800

    创建数据库和配置

---
+++ app/src/main/java/com/gg/coolweather/db/City.java
@@ -0,0 +1,47 @@
+package com.gg.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2018/6/11 0011.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int proviceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProviceId() {
+        return proviceId;
+    }
+
+    public void setProviceId(int proviceId) {
+        this.proviceId = proviceId;
+    }
+
+}
